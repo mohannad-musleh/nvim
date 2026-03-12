907fc621cf149268331f3b8c34c7bf4ec1f7975f
mohannad-musleh <mohannad-musleh@users.noreply.github.com>
2026-03-12 22:10:58 +0200

    Update lsp setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -374,17 +374,15 @@ return { -- LSP Plugins
       })
       require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
+      for server, config in pairs(servers) do
+        if not vim.tbl_isempty(config) then
+          vim.lsp.config(server, config)
+        end
+      end
+
       require('mason-lspconfig').setup({
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
+        ensure_installed = {}, -- explicitly set to an empty table (populates installs via mason-tool-installer)
+        automatic_enable = true, -- automatically run vim.lsp.enable() for all servers that are installed via Mason
       })
     end,
   },

d22fe39112f19cca2bcc570161f5ae1bbc0d906a
mohannad-musleh <mohannad-musleh@users.noreply.github.com>
2026-06-13 21:34:58 +0300

    Follow symlinks in the custom plugins loader

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,7 +1,7 @@
 -- Iterate over all Lua files in the config directory and load them
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'config')
-for file_name, type in vim.fs.dir(plugins_dir) do
-  if type == 'file' and file_name:match '%.lua$' and file_name ~= 'init.lua' then
+for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
+  if (type == 'file' or type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
     local module = file_name:gsub('%.lua$', '')
     require('config.' .. module)
   end
